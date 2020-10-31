e849de443d9c152f6875a214a5a0bec7fc9ec2d4
CodingMirec <m.sykora01@gmail.com>
2020-11-01 00:50:14 +0100

    login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import { getTokenFromUrl } from "./spotify";
 import Login from "./Login";
 
+import "./App.css";
+
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("token >>> ", token);
+  }, []);
+
+  return <div className="app">{token ? <h1>Logged in</h1> : <Login />}</div>;
 }
 
 export default App;
