11dfe54ec5f60759a24025fc6cc82973a7a3678b
CodingMirec <m.sykora01@gmail.com>
2020-11-03 01:05:41 +0100

    added data layer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
 import Login from "./Login";
+import Player from "./Player";
 
 import "./App.css";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -14,12 +18,18 @@ function App() {
 
     if (_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("person", user);
+      });
     }
 
     console.log("token >>> ", token);
   }, []);
 
-  return <div className="app">{token ? <h1>Logged in</h1> : <Login />}</div>;
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
