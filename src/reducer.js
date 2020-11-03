3636c6909f5598c13b694f3a494976295c81d7b2
CodingMirec <m.sykora01@gmail.com>
2020-11-03 01:08:23 +0100

    simple reducer

--- src/reducer.js
+++ src/reducer.js
@@ -5,4 +5,18 @@ export const initialState = {
   item: null,
 };
 
-const reducer = (state, action);
+const reducer = (state, action) => {
+  console.log(action);
+
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
