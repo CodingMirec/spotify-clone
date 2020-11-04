f3ea6161ff73328dbdc4f18bed9c2058fe6f20b8
CodingMirec <m.sykora01@gmail.com>
2020-11-04 18:38:39 +0100

    sidebar nav

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,9 @@
 import React from "react";
+import SidebarOption from "./SidebarOption";
+
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 import "./Sidebar.css";
 
@@ -10,6 +15,9 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt="spotify pic"
       />
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
     </div>
   );
 }
