0ac6fefa2ab24214a645a7403c507fea7cda760c
CodingMirec <m.sykora01@gmail.com>
2020-11-04 22:04:33 +0100

    footer split / playlists dont work

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,30 @@
 import React from "react";
 
+import PlayCircleOutlineOutlinedIcon from "@material-ui/icons/PlayCircleOutlineOutlined";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+
 import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>I am footer</h1>
+      <div className="footer__left">
+        <p>Album and song details</p>
+      </div>
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineOutlinedIcon
+          fontSize="large"
+          className="footer__icon"
+        />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__icon" />
+      </div>
+      <div className="footer_right"></div>
     </div>
   );
 }
