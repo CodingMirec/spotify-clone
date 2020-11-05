3ef80c9484578657a0a6141fd3829fe313a67449
CodingMirec <m.sykora01@gmail.com>
2020-11-05 22:05:43 +0100

    header + discover weekly

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,26 @@
 import React from "react";
 
+import SearchIcon from "@material-ui/icons/Search";
+
 import "./Header.css";
+import { Avatar } from "@material-ui/core";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
-  return <div className="header"></div>;
+  const [{ user }, dispatch] = useStateValue();
+
+  return (
+    <div className="header">
+      <div className="header__left">
+        <SearchIcon />
+        <input placeholder="Search for Artists, Songs..." type="text" />
+      </div>
+      <div className="header__right">
+        <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+        <h4>{user?.display_name}</h4>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
